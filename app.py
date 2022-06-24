01e23e6130721433e6a8deb6f94b35b2bfc4c008
Manuel Kadena <manuelkadena@gmail.com>
2022-06-23 19:56:11 -0500

    proyecto final - codigo base

---
+++ app.py
@@ -0,0 +1,11 @@
+class Calculadora:
+    def sumar(self, num1, num2):
+        return num1 + num2
+
+
+
+instance = Calculadora()
+
+resultado = instance.sumar(10,19)
+
+print("La suma es: ", resultado)
